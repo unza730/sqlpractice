de0ff02813c0d8a66afc76be2f6a5fa1e69c6231
unza730 <unzasohail819@gmail.com>
2023-02-05 00:06:53 +0500

    Day1 Practice to create database

---
+++ SqlQuery/day1.sql
@@ -0,0 +1,11 @@
+-- First We need to create database
+CREATE DATABASE CARS_DATABSE
+
+-- Then Select DataBase From Dropdown and create table
+CREATE TABLE Cars(
+carId INT PRIMARY KEY,
+model varchar(50),
+brand varchar(50),
+color varchar(50),
+make INT
+)
